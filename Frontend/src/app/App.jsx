a5832d0baca4852b7cce2524e6fe4bd540d57ec6
Ankush Pardhi <ankush9pardhi@gmail.com>
2026-01-10 21:57:24 +0530

    app heading and para add

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -7,7 +7,17 @@ function App() {
   return (
     <>
       <div>
-        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+        <h1 class=" p-8 text-3xl font-bold underline">
+          SmartIntern AI , AI-Powered Internship Recommendation & Allocation
+          Platform
+        </h1>
+        <p class="mt-4 text-lg p-6 bg-blue-500 text-white" >
+          The system follows a modular, scalable, and AI-integrated architecture
+          designed to support large-scale internship matching for the PM
+          Internship Scheme and global private companies. It separates user
+          interaction, business logic, and AI intelligence into independent
+          layers to ensure performance, explainability, and ease of integration.
+        </p>
       </div>
     </>
   );
